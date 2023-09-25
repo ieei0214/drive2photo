53109f1d8b64d6fc699b42a5072ddb5658f47ce9
Ricky Liu <ieei.liu@gmail.com>
2023-09-25 11:38:40 -0700

    Add project files.

---
+++ drive2photo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace drive2photo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
